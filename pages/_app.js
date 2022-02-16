a8e975433d7aea165efe664a4361dec053e21f33
Luis Martinez <mrluismartinezzz@gmail.com>
2022-02-16 01:31:15 -0800

    Update _app.js
    wrapping SessionProvider around Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  console.log("session:", session);
+  console.log("pageProps:", pageProps);
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
-
-export default MyApp
