721f22598f36d6a247a188539ee68354ef77145f
Luis Martinez <mrluismartinezzz@gmail.com>
2022-02-16 01:32:10 -0800

    Create [...nextauth].js
    CredentialsProvider to create password & email form

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,47 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      id: "domain-login",
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      type: "credentials",
+      async authorize(credentials, req) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+
+        const res = await fetch("http://localhost:3000/api/hello", {
+          method: "GET",
+          // body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        });
+        const resJson = await res.json();
+        console.log("resJson:", resJson);
+        console.log("LOGGGGIN", credentials);
+        const user = { id: 1, name: "J Smith", email: "jsmith@example.com" };
+
+        // If no error and we have user data, return it
+        if (user) {
+          return user;
+        }
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
+  ],
+  secret: "A_SECRET",
+});
