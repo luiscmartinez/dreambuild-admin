e17c98b6a7fbf9fb5333f6410cf54d180ca5248d
Luis Martinez <mrluismartinezzz@gmail.com>
2022-02-22 19:33:45 -0800

    creation appointment api endpoint post & get
    get all appointments
    create an appointment

--- pages/api/appointments/index.js
+++ pages/api/appointments/index.js
@@ -1,34 +1,28 @@
 import db from "../../../sequelize/models/index.js";
+import { getSession } from "next-auth/react";
 import Cors from "cors";
 
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ["GET", "HEAD"],
-});
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
-
 export default async function handler(req, res) {
-  const { phone, email, body } = req.body;
-  if (phone || email) {
-    const appointment = await db.Appointment.create({
-      phone,
-      email,
-      body,
-    });
-    res.status(200).json({ appointment });
+  if (req.method === "GET") {
+    const session = await getSession({ req });
+    if (!session) {
+      res.status(401).json({ error: "Unauthenticated user" });
+      return;
+    }
+    const appointments = await db.Appointment.findAll();
+    return res.status(200).json({ appointments });
+  } else if (req.method === "POST") {
+    const { phone, email, body } = req.body;
+    if (phone || email) {
+      const appointment = await db.Appointment.create({
+        phone,
+        email,
+        body,
+      });
+      res.status(200).json({ appointment });
+      return;
+    }
+  } else if (req.method === "DELETE") {
     return;
   }
   // bad request needs phone number or email
