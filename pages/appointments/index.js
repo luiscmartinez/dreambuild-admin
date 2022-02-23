a03c8b78735baafa34357335124b6f6c296ce0c6
Luis Martinez <mrluismartinezzz@gmail.com>
2022-02-23 14:10:07 -0800

    updating appointments/index based on peer reviews
    * removing btn to load appointments
    * fetching appointments on component did mount
    * passing arrow function to setState method
    * filterAppointments always returns datatype of arr

--- pages/appointments/index.js
+++ pages/appointments/index.js
@@ -1,29 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../../styles/Appointments.module.css";
 
 function AppointmentsPage() {
   const [appointments, setAppointments] = useState([]);
   const [isSelected, setSelected] = useState(true);
-  const [loading, setLoading] = useState(true);
-  const fetchAppointments = async () => {
+  // for some reason it's standard to init loading to false
+  const [loading, setLoading] = useState(false);
+
+  useEffect(async () => {
     const response = await fetch("/api/appointments");
     const { appointments } = await response.json();
     setAppointments(appointments);
     setLoading(false);
-  };
+  }, []);
+
   const handleSelect = () => {
-    setSelected(!isSelected);
+    //updating state based on current value of state
+    //using functional version in order to guarantee the previous state value before the update invocation.
+    // avoids risk of a stale render
+    setSelected((prev) => !prev);
   };
+
   const filterAppointments = () => {
-    if (appointments.length) {
-      return appointments.filter((a) => {
-        return isSelected ? !a.read : a.read;
-      });
-    }
+    // if appointments is empty arr cause no problem
+    return appointments.filter((a) => {
+      return isSelected ? !a.read : a.read;
+    });
   };
+
   return (
     <>
-      <button onClick={fetchAppointments}>load appointments</button>
       <div className={styles.appointments}>
         <div className={styles.tabs}>
           <div
