fda04e6e57b8077b0c2850d29bf1fe32e68f4c3b
Luis Martinez <mrluismartinezzz@gmail.com>
2022-02-23 19:50:55 -0800

    using radio from antd & handling it's click

--- components/appointments.js
+++ components/appointments.js
@@ -1,9 +1,17 @@
 import styles from "../styles/Appointments.module.css";
+import { Radio } from "antd";
 
 export function Appointments({ appointments }) {
+  const handleClickRead = async (appointmentId) => {
+    await fetch(`/api/appointments/${JSON.stringify(appointmentId)}`, {
+      method: "PUT",
+    });
+  };
+
   if (appointments.length === 0) {
     return <h1>nothing to display here yet</h1>;
   }
+
   return appointments.map((appointment) => {
     const { read, email, id, body } = appointment;
     return (
@@ -11,6 +19,9 @@ export function Appointments({ appointments }) {
         key={id}
         className={read ? styles.appointmentRead : styles.appointmentUnread}
       >
+        <div>
+          <Radio onChange={() => handleClickRead(id)} /> read
+        </div>
         <p>from: {email}</p>
         <p>body: {body}</p>
       </div>
