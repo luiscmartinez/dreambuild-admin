d59d364faf817dcb1c7ab8a0a1abbb0b50f386e5
Luis Martinez <mrluismartinezzz@gmail.com>
2022-03-11 13:05:51 -0800

    sequelize gets initialized by DB URL

--- sequelize/models/index.js
+++ sequelize/models/index.js
@@ -2,15 +2,7 @@ import { Sequelize } from "sequelize";
 import { userFactory } from "./user.js";
 import { appointmentFactory } from "./appointment";
 
-const database = process.env.DB_NAME;
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
-const host = process.env.DB_HOST;
-
-const sequelize = new Sequelize(database, username, password, {
-  host,
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const db = {
   sequelize,
