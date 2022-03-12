440560c6f1d259e8559cde2e4d1badc674d2daec
Luis Martinez <mrluismartinezzz@gmail.com>
2022-03-11 21:28:00 -0800

    Instagram redirect endpoint inserts to DB
    inserts to Integration table

--- pages/api/instagram/index.js
+++ pages/api/instagram/index.js
@@ -4,11 +4,12 @@ import url from "url";
 import db from "../../../sequelize/models/index.js";
 
 export default async function handler(req, res) {
+  const session = await getSession({ req });
   const code = req.query.code;
   const grant_type = "authorization_code";
-  const client_id = process.env.INSTAGRAM_CLIENT_ID;
+  const client_id = process.env.NEXT_PUBLIC_INSTAGRAM_CLIENT_ID;
   const client_secret = process.env.INSTAGRAM_CLIENT_SECRET;
-  const redirect_uri = process.env.INSTAGRAM_REDIRECT_URI;
+  const redirect_uri = process.env.NEXT_PUBLIC_INSTAGRAM_REDIRECT_URI;
 
   const params = new url.URLSearchParams({
     client_id,
@@ -21,17 +22,26 @@ export default async function handler(req, res) {
   axios
     .post(`https://api.instagram.com/oauth/access_token`, params)
     .then((resp) => {
-      const { access_token, user_id } = resp.data;
+      const { access_token, user_id: providerAccountId } = resp.data;
       axios
         .get(
           `https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${client_secret}&access_token=${access_token}`
         )
         .then((response) => {
           console.log("LONG LIVE TOKEN RESPONSE", response.data);
-          const { access_token: accessToken } = response.data;
+          const {
+            access_token: accessToken,
+            token_type: tokenType,
+            expires_in: expiresAt,
+          } = response.data;
+          const userId = String(session.user.id);
+          const provider = "instagram";
           db.Integration.create({
-            id: 1,
-            userId: String(user_id),
+            provider,
+            providerAccountId,
+            tokenType,
+            expiresAt,
+            userId,
             accessToken,
           })
             .then((success) => {
