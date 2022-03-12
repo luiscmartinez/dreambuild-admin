9fca4ef45c24297330a8a177ebc8617a21cf7af5
Luis Martinez <mrluismartinezzz@gmail.com>
2022-03-12 00:29:36 -0800

    displaying appointment creation data

--- components/appointments.js
+++ components/appointments.js
@@ -1,5 +1,8 @@
 import styles from "../styles/Appointments.module.css";
 import { RadioBtn } from "./radioBtn.js";
+import dayjs from "dayjs";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+dayjs.extend(localizedFormat);
 
 export function Appointments({ appointments }) {
   const handleClickRead = async (appointmentId) => {
@@ -13,7 +16,9 @@ export function Appointments({ appointments }) {
   }
 
   return appointments.map((appointment) => {
-    const { read, email, id, body } = appointment;
+    const { read, email, id, body, createdAt } = appointment;
+    const date = new Date(createdAt).toTimeString();
+    const day = createdAt;
     return (
       <div
         key={id}
@@ -22,6 +27,7 @@ export function Appointments({ appointments }) {
         <RadioBtn id={id} handleClickRead={handleClickRead} isRead={read} />
         <p>from: {email}</p>
         <p>message: {body}</p>
+        <p>send on: {dayjs(day).format("LLLL")}</p>
       </div>
     );
   });
