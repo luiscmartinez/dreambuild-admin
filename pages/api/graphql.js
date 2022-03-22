2f7b170ff9d12e05cf94dbfce82b2f502973c707
Luis Martinez <mrluismartinezzz@gmail.com>
2022-03-22 15:04:37 -0700

    init graphQL handler

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,3 +1,49 @@
-export default (req, res) => {
-  res.end("GRAPHQL");
+import { gql, ApolloServer } from "apollo-server-micro";
+
+const typeDefs = gql`
+  type Query {
+    name: String!
+  }
+`;
+// look up the data that is being requested
+const resolvers = {
+  Query: {
+    name: (_parent, _args, _context) => {
+      return "yellow my name is QL!";
+    },
+  },
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startServer = apolloServer.start();
+
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://studio.apollographql.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
+
+  await startServer;
+  await apolloServer.createHandler({
+    path: "/api/graphql",
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 };
